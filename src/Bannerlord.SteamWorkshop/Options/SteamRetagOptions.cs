7034a1e2ecf71fe7c54ff825838c599ca0deef82
Aragas <personal@aragas.org>
2022-11-21 15:09:34 +0200

    Fixed retag

--- src/Bannerlord.SteamWorkshop/Options/SteamRetagOptions.cs
+++ src/Bannerlord.SteamWorkshop/Options/SteamRetagOptions.cs
@@ -1,6 +1,7 @@
 ﻿using CommandLine;
 
 using System.Collections.Generic;
+using System.Linq;
 
 namespace Bannerlord.SteamWorkshop.Options
 {
@@ -8,15 +9,15 @@ namespace Bannerlord.SteamWorkshop.Options
     public class SteamRetagOptions
     {
         [Option('a', "appid", Required = true)]
-        public uint AppId { get; set; } = default!;
+        public uint AppId { get; set; } = 0;
 
         [Option('f', "fileid", Required = true)]
-        public IEnumerable<ulong> FileIds { get; set; } = default!;
+        public IEnumerable<ulong> FileIds { get; set; } = Enumerable.Empty<ulong>();
 
-        [Option('t', "tag", Required = false)]
-        public IEnumerable<string> Tags { get; set; } = default!;
+        [Option('t', "tag", Required = true)]
+        public IEnumerable<string> Tags { get; set; } = Enumerable.Empty<string>();
 
         [Option('d', "description", Required = false)]
-        public string Description { get; set; } = default!;
+        public string Description { get; set; } = string.Empty;
     }
 }
